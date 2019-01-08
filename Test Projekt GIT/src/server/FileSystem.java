8ef339aca744ced8f9a4f3c00da03ac1a17befd5
stbacjon <stbacjon@140600WPC2005.dida140600.local>
2019-01-08 10:10:27 +0100

    Zweiter Commit

---
+++ Test Projekt GIT/src/server/FileSystem.java
@@ -0,0 +1,56 @@
+package server;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+public class FileSystem implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private FilledFolder root;
+	private FilledFile file;
+
+	private void rebuild(FilledFolder ff, String currentRoot) throws IOException {
+		ArrayList<FilledFile> subFiles = ff.getFiles();
+		ArrayList<FilledFolder> subFolders = ff.getSubfolders();
+		
+		//Das ist ein Test
+		try {
+			Files.createDirectory(new File(currentRoot).toPath());
+		}catch (FileAlreadyExistsException e){
+			/* Wenn der Ordner bereits existiert, wird der Inhalt in diesen gespeichert */
+		}
+
+		for (FilledFile file : subFiles) {
+			System.out.println(currentRoot + "\\" + file.getName());
+			File newFile = new File(currentRoot + "\\" + file.getName());
+			newFile.createNewFile();
+			Files.write(newFile.toPath(), file.getContent());
+		}
+		for (FilledFolder folder : subFolders) {
+			rebuild(folder, currentRoot + "\\" + folder.getName());
+		}
+	}
+
+	public void rebuild(String outputPath) throws IOException {
+		if(file == null) {
+			rebuild(root, outputPath);	
+		}else {
+			File newFile = new File(outputPath + "\\" + file.getName());
+			newFile.createNewFile();
+			Files.write(newFile.toPath(), file.getContent());
+		}
+	}
+
+	public FileSystem(String path) throws IOException {
+		File file = new File(path);
+		if(file.isFile()) {
+			this.file = new FilledFile(path);
+		}else {
+			this.root = new FilledFolder(new File(path));	
+		}
+	}
+}
